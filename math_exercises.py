aee618445bafc80c2bd350d77ecbf14d87b21bbf
Renald <r.kronbergs@voco.ee>
2025-09-29 11:05:43 +0300

    Ülesanne: Matemaatilised avaldised- moddli kontroll

--- math_exercises.py
+++ math_exercises.py
@@ -1,46 +1,67 @@
+"""Math exercises."""
 import math
 
-# 1. Sum and difference
-num_a = 5
-num_b = 2
 
-sum = num_a + num_b
-difference = num_a - num_b
+def sum_and_difference(num_a: int, num_b: int) -> tuple:
+    """Return the sum and difference of given variables num_a and num_b."""
+    sum = num_a + num_b
+    difference = num_a - num_b
+    return sum, difference
 
-# Float division
-division = num_a / num_b
 
-# 3. Integer division
+def float_division(num_a: int, num_b: int) -> float:
+    """Divide given variables num_a and num_b and return the result."""
+    division = num_a / num_b
+    return division
 
-division = num_a // num_b
 
-# 4. Powerful operations
+def integer_division(num_a: int, num_b: int) -> int:
+    """Divide given variables num_a and num_b and return the result rounded down."""
+    division = num_a // num_b
+    return division
 
-multiply_numbers = num_a * num_b
-power = num_a ** num_b
-remainder = num_a % num_b
 
-# 5. Find average
+def powerful_operations(num_a: int, num_b: int) -> tuple:
+    """Return the product of given variables, num_a to the power of num_b and the remainder of division of variables."""
+    multiply_numbers = num_a * num_b
+    power = num_a ** num_b
+    remainder = num_a % num_b
+    return multiply_numbers, power, remainder
 
-avarage = (num_a + num_b) / 2
 
-# 6. Area of a circle
+def find_average(num_a: int, num_b: int) -> float:
+    """Return the average of given variables."""
+    average = (num_a + num_b) / 2
+    return average
 
-radius = circle_area = math.pi * radius ** 2
-circle_area = round(circle_area, 2)
 
-# 7. Area of an equilateral triangle
-triangle_area = math.sqrt(3) / 4 * side_length ** 2
-triangle_area = round(triangle_area)
+def area_of_a_circle(radius: float) -> float:
+    """Calculate and return the area of a circle."""
+    radius = circle_area = math.pi * radius ** 2
+    circle_area = round(circle_area, 2)
+    return circle_area
 
-# 8. Calculate discriminant
 
-discriminant = b ** 2 - 4 * a * c
+def area_of_an_equilateral_triangle(side_length: float) -> int:
+    """Calculate and return the area of an equilateral triangle."""
+    triangle_area = math.sqrt(3) / 4 * side_length ** 2
+    triangle_area = round(triangle_area)
+    return triangle_area
 
-# 9. Calculate hypotenuse length
 
-c = math.sqrt(a ** 2 + b ** 2)
+def calculate_discriminant(a: int, b: int, c: int) -> int:
+    """Calculate discriminant with given variables and return the result."""
+    discriminant = b ** 2 - 4 * a * c
+    return discriminant
 
-# 10. Calculate cathetus length
 
-b = math.sqrt(c ** 2 a ** 2)
+def calculate_hypotenuse_length(a: int, b: int) -> float:
+    """Return the length of hypotenuse when the lengths of the catheti are given."""
+    c = math.sqrt(a ** 2 + b ** 2)
+    return c
+
+
+def calculate_cathetus_length(a: int, c: int) -> float:
+    """Return the length of cathetus when the lengths of the second cathetus and hypotenuse are given."""
+    b = math.sqrt(c ** 2 - a ** 2)
+    return b
