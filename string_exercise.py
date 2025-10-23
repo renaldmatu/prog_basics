3c70de058551e6175793e992ede08d27fe0aadee
Renald <r.kronbergs@voco.ee>
2025-10-23 09:03:39 +0300

    Ülesanne: Sõne

---
+++ string_exercise.py
@@ -0,0 +1,16 @@
+first_name = "James"
+last_name = "Bond"
+full_name = f"{first_name} {last_name}"
+self_description_sentence = f"My name is {last_name}, {full_name}."
+cake = "vahukoormarjadtäidispõhi"
+original_string = "Programming is fun!"
+backwards = original_string[-1::-1]
+every_other = original_string[0::2]
+first_word_reversed = first_word_reversed = original_string.split()[0][::-1]
+
+
+print("""vahukoor
+marjad
+täidis
+põhi""")
+
